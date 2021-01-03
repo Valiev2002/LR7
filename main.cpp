b88c6ef930598e4f3921e667766197bb8acd078f
Valiev2002 <70635274+Valiev2002@users.noreply.github.com>
2021-01-03 17:33:42 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,105 +1,102 @@
 #include <iostream>
-using namespace std;
-//Стек работает по правилу портфеля,
-// чтобы достать самую первую вещь,нужно достать все остальные которые идут до 1
-//Вот что я понял,изучая данную тему
-//по программе он похож на очередь,только работаем с конца
-struct element_stack // cтруктура элемент
+//Чтобы показать ,что я пишу сам,я буду объяснять что я делаю\
+//Очередь рабопатьет по принципу первый зашел,первый вышел,как и в живой очереди
+using namespace std;//чтобы не писать каждый раз std
+struct element // создание стурктуры элемент
 {
-    element_stack* prev = nullptr; // указатель на предыдущий элемент
-    int value=3; // значение
+    element* q_next = nullptr;// показывает на след элемиент
+    int value=5 ;//какаое либо значение
 };
 
-struct Stack // структура стек
+struct Queue // структура очередь
 {
-    element_stack* first; // указатель на первый элемент
-    element_stack* last;// указатель на последний элемент
+    element* first;// указатель на первый элемент
+    element* last;// указатель на последний элемент
     int count; // счетчик
 };
 
-void constructor(Stack& stack)// конструктор как в очереди
+void constructor(Queue&lab ) // в конструкторе мы просчто все обнуляем
 {
-    stack.first = nullptr;
-    stack.last= nullptr;
-    stack.count= 0;
+    lab.first = nullptr;
+    lab.last = nullptr;
+    lab.count = 0;
 }
 
-void destructor(Stack& stack)// деструтор
+void destructor (Queue& lab)// деструктор
 {
-    while (stack.last != nullptr)//пока последний элемент не равен 0
+    while (lab.first != nullptr)//пока первый элеиент не равен 0
     {
-        element_stack* tmp;//указатль
-        tmp=stack.last;//присваеваем ему значение последнего элемента
-        stack.last=stack.last -> prev;//обращаемся к предыдущему жлементу
-        delete tmp;//удаляем присвоенное значение
-        stack.count --;//уменьшаем счетчик на 1
+        element* temp = temp = lab.first;//присваиваем значение временной переменной
+        lab.first = lab.first -> q_next;
+        delete temp;//удаляем ,потому что это обязательно для динамического выделения памяти
+        lab.count --;//уменьшаем на 1
+    }
+}
+void push(Queue& myQueue, element& bmw)// функция заполнения
+{
+    auto* q = new element;// динамически выделяем память
+    q -> value = bmw.value; //обращение к элементу значения
+    if (myQueue.first == nullptr)// если первый жлемент равен 0
+    {
+        myQueue.first = q;//присваиваем значение для первого элемента
+        myQueue.last = q;//присваиваем значение для последнего элемента
+    } else
+    {
+        myQueue.last -> q_next = q;// присваваем временной пременной следующее значения
+        myQueue.last = q;// присваеваем конец очереди к этой переменной
+    }
+    myQueue.count ++;// шаг 1(увеличение)
+
+}
+
+void pop(Queue &myQueue)//удаление
+{
+    element  *temp = myQueue.first;// временная переменная
+    myQueue.first = myQueue.first -> q_next;//список начинается со второго элемента
+    delete temp;// удаляем первый элемент, записанный во временную переменную
+    myQueue.count --;// уменьшаем на 1
+}
+int size(Queue& myQueue)// размер
+{
+  return myQueue.count;//счетчик
+}
+
+void print(Queue& myQueue)//  печать
+{
+    element* temp = myQueue.first;//выделяем память
+    while (temp != nullptr)//пока эта переемнная не равна 0
+    {
+        int element = temp -> value;//значение из первого элемента
+        cout << element << " ";
+        temp = temp -> q_next;// переходим к следующему элементу
     }
 }
 
-unsigned int size(Stack& stack)//размер(просто показываем счетчик)
-{
-    return stack.count;
-}
 
-void push(Stack& stack, element_stack& element)//вставить
+int main()
 {
-    auto* tmp = new element_stack;// динамическое выделение памяти
-    tmp -> value = element.value;//обратимся к значению
-    if (stack.first == nullptr)//если первый равен 0
+    Queue VQueue{};
+    constructor(VQueue);
+    element Element;
+    int j;
+    int q;
+    cout << "Enter how many elements will be: ";
+    cin >> q;
+    for (int i = 1; i <= q; ++i)
     {
-        stack.first = tmp;
-        stack.last = tmp;
+        cout << "enter " << i << " element:";
+        cin >> j;
+        Element.value = j;
+        push(VQueue, Element);
     }
-    else//если какой то другой случай
-    {
-        tmp -> prev =stack.last;// образаемся к предвдущему эдеиенту и делаем его последним
-        stack.last = tmp;// присваеваем  конец
-    }
-    stack.count ++; // уевличиваем на 1
+    print(VQueue);
+    cout << endl;
+    cout << "Size is : " << size(VQueue) << endl;
+    cout << "delete the first element: " << VQueue.first -> value << endl;
+    pop(VQueue);
+    cout << "Now size is : " << size(VQueue) << endl;
+    print(VQueue);
+    destructor(VQueue);
+    cout << "there are " << size(VQueue) << endl;
+    return 0;
 }
-element_stack pop(Stack& stack)// удаляем
-{
-    element_stack  *temp = stack.last;// временная переменная,УДАЛИ
-    stack.last = stack.last -> prev;//все как в очереди,только с конца,второй элемент теперь последний
-    delete temp;// удаляем
-    stack.count --;// уменьшаем счетчик
-}
-
-void print(Stack& myStack)// печать
-{
-    element_stack *temp = myStack.last;//указатель на последний элемент
-    while (temp != nullptr)//пока не равен 0
-    {
-        int element = temp->value;//значение пред элемента
-        cout << element << endl;
-        temp = temp->prev;// переходим к предыдущему элементу
-    }
-}
-
-    int main()
-    {
-        Stack stack{};
-        constructor(stack);
-        destructor(stack);
-        element_stack element;
-        int h;
-        int c;
-        cout << "Enter how many elements will be  = ";//привет
-        cin >> c;
-        for (int i = 0; i <= c; ++i)//учитывая 0
-        {
-            cout << "enter " << i<<" : " ;//для пользователя
-            cin >> h;
-            element.value =h;
-            push(stack, element);
-        }
-        print(stack);
-        cout << endl;
-        cout << "Size is " << size(stack) << endl;
-        cout << "delete last element: " << stack.first->value << endl;
-        pop(stack);
-        cout << "now size is " << size(stack) << endl;
-        print(stack);
-
-        return 0;
-    }
